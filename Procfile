54c94766f62fb2a96c44dc0720dff877d9b78165
asliddintursunoff <asliddintursunoff@icloud.com>
2025-06-23 21:03:19 +0500

    Repairing static file

--- Procfile
+++ Procfile
@@ -1,2 +1,8 @@
-web: python manage.py makemigrations && python manage.py migrate && python manage.py create_superuser && gunicorn project.wsgi && python manage.py collectstatic --noinput
+# Runs only once during deployment
+release: python manage.py makemigrations && python manage.py migrate && python manage.py collectstatic --noinput
+
+# Main web server
+web: gunicorn project.wsgi:application --log-file -
+
+# Background Telegram bot process
 worker: python telegram_bot.py
--- project/settings.py
+++ project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -144,3 +146,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
