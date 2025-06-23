156d50c813308885e410622bc0fe5c496c6bde77
asliddintursunoff <asliddintursunoff@icloud.com>
2025-06-23 17:56:50 +0500

    Prepare for Railway deployment

--- project/settings.py
+++ project/settings.py
@@ -19,13 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sbt3^rgmm3vzo)23w0bi@-+6)q0d!**+cs9+gk!0#zv-yj7y&i'
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = ['*']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
